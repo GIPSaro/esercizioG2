58442716bed65674f1c7c5677063ec8c73cd585a
Giorgia Ipsaro Passione <145654021+GIPSaro@users.noreply.github.com>
2023-11-29 08:56:22 +0100

    commit

---
+++ src/app/components/post-attivi/post-attivi.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Post } from 'src/app/models/post';
+import { PostsServicesService } from 'src/app/services/post.service';
+@Component({
+  selector: 'app-post-attivi',
+  templateUrl: './post-attivi.component.html',
+  styleUrls: ['./post-attivi.component.scss'],
+})
+export class PostAttiviComponent implements OnInit {
+  posts!: Post[];
+
+  constructor(private postsSrv: PostsServicesService) {}
+
+  ngOnInit(): void {
+    this.postsSrv.getIt().then((data) => {
+      this.posts = data;
+      console.log(this.posts);
+    });
+  }
+  disable(post: Post) {
+    post.active = false;
+  }
+}
